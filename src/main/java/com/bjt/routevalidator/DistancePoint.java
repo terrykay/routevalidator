4364ddd679806fb00ee638491881cc8483859249
Ben Taylor <ben.taylor@ssp-uk.com>
2016-05-13 12:56:37 +0100

    fix time zone issue (?)

--- src/main/java/com/bjt/routevalidator/DistancePoint.java
+++ src/main/java/com/bjt/routevalidator/DistancePoint.java
@@ -1,5 +1,9 @@
 package com.bjt.routevalidator;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDateTime;
+
 /**
  * Created by Ben.Taylor on 23/04/2016.
  */
@@ -9,12 +13,18 @@ public class DistancePoint {
     private double lng;
 
     public DistancePoint(final TrackpointWrapper trackpointWrapper) {
+        final DateTime dateTime = trackpointWrapper.getDateTime();
         label = String.format( "%.1f km @ %s (%s)",
                 trackpointWrapper.getDistanceCumulative(),
-                trackpointWrapper.getDateTime().toString("h:mm a").toLowerCase(),
+                formatDateTime(dateTime),
                 DurationStatistic.getDurationString(trackpointWrapper.getTimeCumulative().toPeriod())
         );
         lat = trackpointWrapper.getTrackpoint().getLat();
         lng = trackpointWrapper.getTrackpoint().getLon();
+    }
+
+    public static String formatDateTime(final DateTime dateTime) {
+        final DateTime localDateTime = dateTime.withZone(DateTimeZone.getDefault());
+        return localDateTime.toString("h:mm a").toLowerCase();
     }
 }
--- src/test/java/FeedbackTest_TimeCalcs.java
+++ src/test/java/FeedbackTest_TimeCalcs.java
@@ -1,5 +1,6 @@
 import com.bjt.gpxparser.GeoFileParser;
 import com.bjt.routevalidator.*;
+import org.joda.time.DateTime;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -22,5 +23,13 @@ public class FeedbackTest_TimeCalcs {
         final InputStream inputStream = getClass().getResourceAsStream(name);
         final GeoFileParser gpxparser = new GeoFileParser();
         return new GpxFile(name, gpxparser.parseGeoFile(inputStream, name));
+    }
+
+    @Test
+    public void utcTimeShowsAsBst() { //UTC is 'equivalent' to GMT, and is indicated by the Z suffix.. should show as BST (it's in may)
+        final DateTime utcTime = DateTime.parse("2016-04-30T05:09:04Z");
+        final String formattedDateTime = DistancePoint.formatDateTime(utcTime);
+
+        Assert.assertEquals("6:09 am", formattedDateTime);
     }
 }
