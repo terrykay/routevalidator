f5b2e2711da69c6bf4d0455f9720ea14732dbc0e
Ben Taylor <ben.taylor@ssp-uk.com>
2016-06-09 15:55:59 +0100

    add tests for too-long duration

--- src/test/java/DurationStatisticTest.java
+++ src/test/java/DurationStatisticTest.java
@@ -38,4 +38,34 @@ public class DurationStatisticTest {
         String duration = DurationStatistic.getDuration(tcxTime1, tcxTime2);
         Assert.assertEquals("25:01", duration);
     }
+
+    @Test
+    public void oneWeekApart() {
+        String tcxTime1 = "2010-01-01T00:00:00Z";
+        String tcxTime2 = "2010-01-08T00:00:00Z";
+        String duration = DurationStatistic.getDuration(tcxTime1, tcxTime2);
+        Assert.assertEquals("168:00", duration);
+
+    }
+    @Test
+    public void threeWeeksApart() { //should be ok
+        String tcxTime1 = "2010-01-01T00:00:00Z";
+        String tcxTime2 = "2010-01-22T00:00:00Z";
+        String duration = DurationStatistic.getDuration(tcxTime1, tcxTime2);
+        Assert.assertEquals("504:00", duration);
+
+    }
+
+    @Test
+    public void moreThanAMonthApart() { //shouldn't ever have to encounter this.. .but should throw meaningful exception rather than a silly one
+        String tcxTime1 = "2010-01-01T00:00:00Z";
+        String tcxTime2 = "2010-02-01T00:00:00Z";
+        try {
+            String duration = DurationStatistic.getDuration(tcxTime1, tcxTime2);
+            Assert.fail("Exception was not thrown...duration was apparently " + duration);
+        }catch(final Exception e) {
+            Assert.assertEquals("Times in the actual track(s) were too far (more than a month) apart.", e.getMessage());
+        }
+
+    }
 }
--- src/test/java/FeedbackTestZip.java
+++ src/test/java/FeedbackTestZip.java
@@ -1,9 +1,29 @@
+import com.bjt.gpxparser.GeoFile;
+import com.bjt.gpxparser.GeoFileParser;
+import com.bjt.routevalidator.DurationStatistic;
+import com.bjt.routevalidator.TrackSummary;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.InputStream;
 
 /**
  * Created by Ben.Taylor on 09/06/2016.
  */
 public class FeedbackTestZip {
     @Test
-    public void
+    public void DurationStatisticFailsToParse() throws Exception {
+
+        final GeoFile geoFile = getGeoFile("/Refer1and2.zip");
+
+        final String duration = DurationStatistic.getDuration(geoFile);
+
+    }
+
+    private GeoFile getGeoFile(String name) throws Exception {
+        final InputStream inputStream = getClass().getResourceAsStream(name);
+        final GeoFileParser gpxparser = new GeoFileParser();
+        return gpxparser.parseGeoFile(inputStream, name);
+    }
+
 }
